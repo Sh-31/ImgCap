0cf0bf01d795732c30c8186df73157d7f7bb34f1
sherif ahmed <sherif201665@gmail.com>
2024-08-25 17:54:21 +0000

    update

--- trainning/model.py
+++ trainning/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torchvision.models as models
+
 ## ResNet50 (CNN Encoder)
 
 class ResNet50(nn.Module):
@@ -8,9 +9,10 @@ class ResNet50(nn.Module):
         self.ResNet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
 
         self.ResNet50.fc = nn.Sequential(
-                            nn.Linear(2048, 512),
+                            nn.Linear(2048, 1024),
                             nn.ReLU(),
-                            nn.Linear(512, 1024),
+                            nn.Dropout(0.5),
+                            nn.Linear(1024, 1024),
                             nn.ReLU(),
                            )
 
@@ -21,7 +23,7 @@ class ResNet50(nn.Module):
             v.requires_grad = False
 
     def forward(self,x):
-        return self.ResNet50(x)        
+        return self.ResNet50(x)       
 
 ## lSTM (Decoder)
 
@@ -35,7 +37,6 @@ class lstm(nn.Module):
         self.embedding_dim = embedding_dim
         self.vocab_size = vocab_size
         
-        
         self.embedding = nn.Embedding(vocab_size, hidden_size)
         self.projection = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
@@ -44,7 +45,7 @@ class lstm(nn.Module):
             input_size=hidden_size,
             hidden_size=hidden_size,
             num_layers=number_layers,
-            dropout=0.2,
+            dropout=0.5,
             batch_first=True,
         )
 
@@ -53,14 +54,12 @@ class lstm(nn.Module):
     def forward(self, x, captions):
         projected_image = self.projection(x).unsqueeze(dim=1)
         embeddings = self.embedding(captions[:, :-1])
-        
-
+    
         # Concatenate the image feature as frist step with word embeddings
         lstm_input = torch.cat((projected_image, embeddings), dim=1)
-        # print(torch.all(projected_image[:, 0, :] == lstm_input[:, 0, :]))
-    
+        # print(torch.all(projected_image[:, 0, :] == lstm_input[:, 0, :])) # check
+        
         lstm_out, _ = self.lstm(lstm_input)
-
         logits = self.fc(lstm_out)
 
         return logits
@@ -122,3 +121,111 @@ class ImgCap(nn.Module):
         return captions
 
 
+### Other Implementations FOR LSTM ###
+
+
+  # def forward(self, x, captions):
+  #     batch, seq_len = captions.shape
+  #     h0 = torch.zeros(batch, self.hidden_size).to(x.device)
+  #     c0 = torch.zeros(batch, self.hidden_size).to(x.device)
+
+  #     logits = torch.zeros(batch, seq_len, self.vocab_size).to(x.device)
+
+  #     h, c = self.lstm(x, (h0, c0))
+  #     output = self.fc(h)
+  #     logits[:, 0, :] = output
+
+  #     for t in range(1, seq_len):
+  #       word_embedding = self.embedding(captions[:, t-1])
+  #       h, c = self.lstm(word_embedding, (h, c))
+  #       output = self.fc(h)
+  #       logits[:, t, :] = output
+
+  #     return logits
+
+
+    # def forward(self, x, captions):
+    #     # Implementation without Teacher Forcing and using the word probability
+    #     batch_size = x.shape[0]
+    #     seq_len = captions.size(1)
+
+    #     h0 = torch.zeros(batch_size, self.hidden_size).to(x.device)
+    #     c0 = torch.zeros(batch_size, self.hidden_size).to(x.device)
+
+    #     logits = torch.zeros(batch_size, seq_len, self.vocab_size).to(x.device)
+
+    #     h, c = self.lstm(x, (h0, c0))
+    #     logit = self.fc(h)
+    #     probs = F.softmax(logit, dim=1)
+    #     predicted_word_index = torch.argmax(probs, dim=1)
+
+    #     logits[:, 0, :] = logit
+
+    #     for t in range(1, seq_len):
+    #         word_embedding = self.embedding(predicted_word_index)
+    #         h, c = self.lstm(word_embedding, (h, c))
+    #         h = self.dropout(h)
+    #         logit = self.fc(h)
+
+    #         probs = F.softmax(logit, dim=1)
+    #         predicted_word_index = torch.argmax(probs, dim=1)
+    #         logits[:, t, :] = logit
+
+    #     return logits
+
+
+
+# class Lstm(nn.Module):
+#     def __init__(self, input_size, hidden_size, number_layers, embedding_dim, vocab_size):
+#         super(Lstm, self).__init__()
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.number_layers = number_layers
+#         self.embedding_dim = embedding_dim
+#         self.vocab_size = vocab_size
+
+#         self.embedding = nn.Embedding(vocab_size, embedding_dim)
+
+#         # First LSTMCell
+#         self.lstm1 = nn.LSTMCell(input_size=embedding_dim, hidden_size=hidden_size)
+
+#         # Second LSTMCell
+#         self.lstm2 = nn.LSTMCell(input_size=hidden_size, hidden_size=hidden_size)
+
+#         self.fc = nn.Linear(hidden_size, vocab_size)
+#         self.dropout = nn.Dropout(0.2)
+
+#     def init_hidden_and_cell_states(self, batch_size, device):
+#         h = torch.zeros(batch_size, self.hidden_size).to(device)
+#         c = torch.zeros(batch_size, self.hidden_size).to(device)
+#         return h, c
+
+#     def forward(self, x, captions):
+#         batch, seq_len = captions.shape
+
+#         h1, c1 = self.init_hidden_and_cell_states(batch, x.device)
+#         h2, c2 = self.init_hidden_and_cell_states(batch, x.device)
+
+#         logits = torch.zeros(batch, seq_len, self.vocab_size).to(x.device)
+
+#         x = self.dropout(x)
+
+#         h1, c1 = self.lstm1(x, (h1, c1))
+#         h2, c2 = self.lstm2(h1, (h2, c2))
+
+#         output = self.fc(h2)
+#         logits[:, 0, :] = output
+
+#         for t in range(1, seq_len):
+#             word_embedding = self.embedding(captions[:, t-1])
+#             word_embedding = self.dropout(word_embedding)
+
+#             # First LSTMCell
+#             h1, c1 = self.lstm1(word_embedding, (h1, c1))
+#             # Second LSTMCell
+#             h2, c2 = self.lstm2(h1, (h2, c2))
+
+#             output = self.fc(h2)
+#             logits[:, t, :] = output
+
+#         return logits
--- trainning/trainer.py
+++ trainning/trainer.py
@@ -1,28 +1,38 @@
-###Tranning loop###
-
 import time
+import pickle
+import numpy as np
+import torch
+import torch.nn as nn
 import torch.optim as optim
 from torch.cuda.amp import autocast, GradScaler
-import torch.nn as nn
-import numpy as np
+import torchvision.transforms as T
+from torch.utils.data import random_split, DataLoader
+from model import ImgCap
+from torch.utils.tensorboard import SummaryWriter
 
-def train_one_epoch(model, train_loader, criterion, optimizer, scaler, device):
+import sys
+sys.path.append('../')
+from data_utils import Flickr30, collate_fn
+from eval_utils import eval_bleu_score, eval_CIDEr
+
+
+def train_one_epoch(model, train_loader, criterion, optimizer, scaler, device, epoch, writer):
     model.train()
     loss_running = []
     norm_avg = []
 
-    for images, captions in train_loader:
+    for batch_idx, (images, captions) in enumerate(train_loader):
         images, captions = images.to(device), captions.to(device)
         optimizer.zero_grad()
 
         with autocast(dtype=torch.float16):
             outputs = model(images, captions)
             try:
-                loss = criterion(outputs.view(-1, outputs.size(2)), captions.contiguous().view(-1)) # outputs -> (B*S, vocab_size) , captions -> (B * S)
+                loss = criterion(outputs.view(-1, outputs.size(2)), captions.contiguous().view(-1))  # outputs -> (B*S, vocab_size) , captions -> (B * S)
             except Exception as e:
                 print(f"Error during training: {e}")
                 print(f"Batch size: {captions.shape[0]}, Sequence length: {captions.shape[1]}")
-                print(f"Outputs shape: {outputs.shape},  Captions shape: {captions.shape}")
+                print(f"Outputs shape: {outputs.shape}, Captions shape: {captions.shape}")
                 exit()
 
         scaler.scale(loss).backward()
@@ -33,10 +43,13 @@ def train_one_epoch(model, train_loader, criterion, optimizer, scaler, device):
         norm_avg.append(norm.item())
         loss_running.append(loss.item())
 
+        writer.add_scalar('Train/Loss', loss.item(), epoch * len(train_loader) + batch_idx)
+        writer.add_scalar('Train/Gradient Norm', norm.item(), epoch * len(train_loader) + batch_idx)
+
     return np.mean(loss_running), np.mean(norm_avg)
 
 
-def validate_model(model, val_loader, criterion, vocab, decoder, device):
+def validate_model(model, val_loader, criterion, vocab, decoder, device, writer, epoch):
     model.eval()
     val_loss = []
     bleu_scores = []
@@ -59,23 +72,42 @@ def validate_model(model, val_loader, criterion, vocab, decoder, device):
                 bleu_scores.append(bleu4_score)
                 cider_scores.append(cider_score)
 
-    return np.mean(val_loss), np.mean(bleu_scores), np.mean(cider_scores), generated_captions, decoded_captions
+    avg_val_loss = np.mean(val_loss)
+    avg_bleu = np.mean(bleu_scores)
+    avg_cider = np.mean(cider_scores)
+
+    writer.add_scalar('Validation/Loss', avg_val_loss, epoch)
+    writer.add_scalar('Validation/BLEU', avg_bleu, epoch)
+    writer.add_scalar('Validation/CIDEr', avg_cider, epoch)
+
+    return avg_val_loss, avg_bleu, avg_cider, generated_captions, decoded_captions
 
 
-def train_model(model, train_loader, val_loader, criterion, optimizer, scaler, num_epochs, device, vocab, decoder):
+def train_model(model, train_loader, val_loader, criterion, optimizer, scaler, num_epochs, device, vocab, decoder, checkpoint_path, log_dir):
+    writer = SummaryWriter(log_dir=log_dir)
+
     for epoch in range(num_epochs):
         start_time = time.time()
 
-        train_loss, norm = train_one_epoch(model, train_loader, criterion, optimizer, scaler, device)
+        train_loss, norm = train_one_epoch(model, train_loader, criterion, optimizer, scaler, device, epoch, writer)
         epoch_time = time.time() - start_time
 
-        if (epoch + 1) % 5 == 0:
+        if (epoch + 1) % 10 == 0:
             val_loss, avg_bleu, avg_cider, generated_captions, decoded_captions = validate_model(
-                model, val_loader, criterion, vocab, decoder, device
+                model, val_loader, criterion, vocab, decoder, device, writer, epoch
             )
 
-            epoch_time = time.time() - start_time
+            checkpoint = {
+                'epoch': epoch + 1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': train_loss,
+                'val_loss': val_loss,
+                'bleu_score': avg_bleu,
+                'cider_score': avg_cider,
+            }
 
+            torch.save(checkpoint, f'{checkpoint_path}/checkpoint_epoch_{epoch + 1}.pth')
 
             print(f"Epoch [{epoch + 1}/{num_epochs}], "
                   f"Train Loss: {train_loss:.4f}, "
@@ -88,29 +120,71 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, scaler, n
             print(f"Generated Caption Example: {generated_captions[0]}")
             print(f"Ground Truth Caption Example: {decoded_captions[0]}")
             print("-" * 120)
+
         else:
             print(f"Epoch [{epoch + 1}/{num_epochs}], "
                   f"Train Loss: {train_loss:.4f}, "
                   f"Norm: {norm:.2f}, "
                   f"Epoch Time: {epoch_time:.2f} sec")
 
+            checkpoint = {
+                'epoch': epoch + 1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'train_loss': train_loss,
+            }
+            
+            torch.save(checkpoint, f'{checkpoint_path}/checkpoint_epoch_{epoch + 1}.pth')
+      
+
+    writer.close()
     return model, optimizer, train_loss, val_loss
 
 
-# Initialize training components
-decoder = dataset.decoder
-vocab = dataset.vocab
+if __name__ == "__main__":
 
-criterion = nn.CrossEntropyLoss(ignore_index=vocab['<pad>'])
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-scaler = GradScaler()
+    root_path = '/teamspace/studios/this_studio/ImgCap'
+    Flickr30_image_path = f"{root_path}/data/Flickr30/imges"
+    Flickr30_labels_path = f"{root_path}/data/Flickr30/results.csv"
+    vocab_path = f"{root_path}/vocab.pkl"
 
-model = ImgCap(cnn_feature_size=1024, lstm_hidden_size=512, embedding_dim=512, num_layers=1, vocab_size=1024)
-optimizer = optim.AdamW(model.parameters(), lr=1e-4)
-model.to(device)
+    transforms = T.Compose([
+        T.ToPILImage(),
+        T.Resize((224, 224)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
 
-num_epochs = 35
+    with open(vocab_path, 'rb') as f:
+        vocab = pickle.load(f)
 
-model, optimizer, train_loss, val_loss = train_model(
-    model, train_data_loader, val_data_loader, criterion, optimizer, scaler, num_epochs, device, vocab, decoder
-)
+    dataset = Flickr30(Flickr30_image_path, Flickr30_labels_path, vocab=vocab, transform=transforms)
+
+    train_size = int(0.90 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_data_loader = DataLoader(train_dataset, batch_size=256, shuffle=True, collate_fn=collate_fn)
+    val_data_loader = DataLoader(val_dataset, batch_size=256, shuffle=True, collate_fn=collate_fn)
+
+    decoder = dataset.decoder
+    vocab = dataset.vocab
+
+    criterion = nn.CrossEntropyLoss(ignore_index=vocab['<pad>'])
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    scaler = GradScaler()
+
+    model = ImgCap(cnn_feature_size=1024, lstm_hidden_size=1024, embedding_dim=1024, num_layers=2, vocab_size=len(vocab))
+
+    optimizer = optim.AdamW(model.parameters(), lr=3e-4)
+    model.to(device)
+
+    num_epochs = 250
+
+    checkpoint_path = f"{root_path}/trainning/checkpoints"
+    log_dir = f"{root_path}/trainning/logs"
+
+    model, optimizer, train_loss, val_loss = train_model(
+        model, train_data_loader, val_data_loader, criterion, optimizer, scaler, num_epochs, device, vocab, decoder,
+        checkpoint_path, log_dir
+    )
